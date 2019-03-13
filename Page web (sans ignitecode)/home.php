4cd52f47c26884f05212712509ca2839e5a6f229
Jofresh <46541094+Jofresh@users.noreply.github.com>
2019-03-13 16:13:04 +0100

    Update home.php

--- Page web (sans ignitecode)/home.php
+++ Page web (sans ignitecode)/home.php
@@ -1,9 +1,26 @@
+<?php 
+	session_start();
+
+	if (!isset($_SESSION['login'])) {
+		header("location: login.php");
+	}
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<title>sqdqsdqsd</title>
-</head>
-<body>
-qsdqsdqdqsdqsdqsd
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>home.php</title>
+		<link rel="stylesheet" type="text/css" href="css/home.css">
+	</head>
+	<body>
+		<p>Welcome <?php echo $_SESSION['username']; ?></p>
+		<?php 
+			if (isset($_SESSION['message'])) {
+				echo "<div id='error'>".$_SESSION['message']."</div>";
+				unset($_SESSION['message']);
+			}
+		?>
+		<p><a href="logout.php">Log out</a></p>
+	</body> 
 </html>

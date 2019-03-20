74c298465367972f17296c249c97484480e4403f
Jofresh <46541094+Jofresh@users.noreply.github.com>
2019-03-20 16:29:51 +0100

    Update home.php

--- Page web (sans ignitecode)/home.php
+++ Page web (sans ignitecode)/home.php
@@ -4,13 +4,24 @@
 	if (!isset($_SESSION['login'])) {
 		header("location: login.php");
 	}
+
+	function creer_un_pokemon($num_dresseur, $num_pokemon) {
+		$db = mysqli_connect('https://***REMOVED***', '***REMOVED***', '***REMOVED***', '***REMOVED***');
+		$query = "INSERT INTO Pokemon_des_dresseurs(Num_dresseur, Num_pokemon, IV_PV, IV_Attaque, IV_Defense, IV_Attaque_Spe, IV_Defense_Spe, IV_Vitesse) VALUES(".$num_dresseur.", ".$num_pokemon.", ".rand(0,31).", ".rand(0,31).", ".rand(0,31).", ".rand(0,31).", ".rand(0,31).", ".rand(0,31).")";
+
+		mysqli_query($db, $query);
+		mysqli_close($db);
+	}
+
+	creer_un_pokemon(2, 1);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>home.php</title>
+		<title>Pokemon</title>
 		<link rel="stylesheet" type="text/css" href="css/home.css">
 		<link rel="icon" href="icon.ico" />
 	</head>

ca74ab335c102a3c97d71bb73b7ea9f2b7834887
Jofresh <46541094+Jofresh@users.noreply.github.com>
2019-04-03 12:00:58 +0200

    Update starter_selection.php

--- Page web (sans ignitecode)/starter_selection.php
+++ Page web (sans ignitecode)/starter_selection.php
@@ -1,20 +1,28 @@
 <?php
+
 session_start();
 include 'fonctions.php';
-if(!besoin_de_starter($_SESSION['num_user'])){
-  header("location: home.php");
-  exit();
+
+if (!besoin_de_starter($_SESSION['num_user'])) {
+    header("location: home.php");
+    exit();
 }
- ?>
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <title>Pokemon - Starter</title>
-  </head>
-  <body>
-    <a href="redirection.php?starter=1">Bulbizarre</a>
-    <a href="redirection.php?starter=2">Salamèche</a>
-    <a href="redirection.php?starter=3">Carapuce</a>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Pokemon - Starter</title>
+        <link rel="stylesheet" type="text/css" href="css/starter.css">
+    </head>
+    <body>
+        <!-- Bulbizarre -->
+        <a href="redirection.php?starter=1" id="blbzr"><img src="css/bg1.png"></a>
+        <!-- Salamèche -->
+        <a href="redirection.php?starter=4" id="slm"><img src="css/bg2.png"></a>
+        <!-- Carapuce -->
+        <a href="redirection.php?starter=8" id="crpc"><img src="css/bg3.png"></a>
+    </body>
 </html>

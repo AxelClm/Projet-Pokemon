fe0d6cd40076bd9b76b9db971db63c5ad7a33780
Jofresh <46541094+Jofresh@users.noreply.github.com>
2019-04-03 11:10:16 +0200

    Update reward.php

--- Page web (sans ignitecode)/reward.php
+++ Page web (sans ignitecode)/reward.php
@@ -1,30 +1,35 @@
 <?php
+
 session_start();
+
 include 'fonctions.php';
-if(need_reward($_SESSION['num_user'])){
+
+if (need_reward($_SESSION['num_user'])) {
     give_reward_journaliere($_SESSION['num_user']);
     update_last_reward($_SESSION['num_user']);
 }
-else{
+else {
     header("location: home.php");
     exit();
 }
 
+?>
 
-
-
-
- ?>
-
- <!DOCTYPE html>
- <html lang="fr">
-   <head>
-     <meta charset="utf-8">
-     <title>Pokemon - Reward</title>
-   </head>
-   <body>
-     Vous avez obtenu 50 Pokedollar et 5 Pokeball.
-     <br>
-     <a href="home.php">Continuer</a>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Pokemon - Récompense</title>
+        <link rel="stylesheet" type="text/css" href="css/reward.css">
+        <link rel="icon" href="icon.ico"/>
+    </head>
+    <body>
+        <div class="reward-box">
+            <h1>Récompense du jour</h1>
+            <div class="reward">
+                Vous avez obtenu 50₽ et 5 pokéballs
+            </div>
+            <a href="home.php" id="home"><input type="button" value="Continuer" class="btn"/></a>
+        </div>
    </body>
- </html>
+</html>

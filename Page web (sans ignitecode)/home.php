aad35441235a67f67b977095be3aa121fc724c41
AxelClm <46006583+AxelClm@users.noreply.github.com>
2019-04-07 17:04:54 +0200

    Add files via upload

--- Page web (sans ignitecode)/home.php
+++ Page web (sans ignitecode)/home.php
@@ -6,14 +6,14 @@ if (!isset($_SESSION['num_user']) || !isset($_SESSION['login'])) {
 }
 
 include 'fonctions.php';
-    //Mise a jour de la derniere connection
+	//Mise a jour de la derniere connection
 is_connected($_SESSION['num_user']);
-    //redirection vers la page de selection des starters si besoin
+	//redirection vers la page de selection des starters si besoin
 if(besoin_de_starter($_SESSION['num_user']) == 1){
     header("location: starter_selection.php");
     exit();
 }
-    //redirection vers la page des recompenses
+	//redirection vers la page des recompenses
 if (need_reward($_SESSION['num_user'])){
     header("location: reward.php");
     exit();
@@ -31,10 +31,10 @@ if (need_reward($_SESSION['num_user'])){
     </head>
 
     <body>
-        <p>Bienvenue <?php echo $_SESSION['username']; ?> !</p>
+        <p>Welcome <?php echo $_SESSION['username']; ?></p>
         <div class="friends">
             <a href="add_friend.php">Ajouter un ami</a>
-            <h3>Vos amis</h6>
+            <h3>Vos amis</h3>
             <div class="demande_reçus">
             </div>
             <div class="online_friends">
@@ -46,13 +46,12 @@ if (need_reward($_SESSION['num_user'])){
         </div>
         <div id="selection">
             <div id="gerer_equipe">
-                <p onclick="afficher_gerer_equipe()">Gérer votre équipe</p>
+                <p onclick="afficher_gerer_equipe()">Gerer Equipe</p>
             </div>
         </div>
-        <p><a href="redirection.php?disconnect=true">Se déconnecter</a></p>
+        <p><a href="redirection.php?disconnect=true">Log out</a></p>
     </body>
 </html>
-
 <script>
     function accepter_demande(num){
         var xhr = new XMLHttpRequest
@@ -84,8 +83,7 @@ if (need_reward($_SESSION['num_user'])){
                 var tab = JSON.parse(xhr.responseText);
                 var text="";
                 for (var i = 0;i<tab.length;i++){
-                    text = text+"<div><p>"+tab[i]['username']+"</p><p id='demande_add_"+tab[i]['id']+"' class='btn'>Accepter</p>"
-               +"<p id='demande_refus_"+tab[i]['id']+"' class='btn'>Refuser</p></div>";
+                  text = text+"<div><p>"+tab[i]['username']+"</p><p id='demande_add_"+tab[i]['id']+"' class='btn'>Accepter</p>"+"<p id='demande_refus_"+tab[i]['id']+"' class='btn'>Refuser</p></div>";
                 }
                 document.querySelector(".demande_reçus").innerHTML = text;
                 for (var j = 0;j<tab.length;j++){
@@ -143,26 +141,28 @@ if (need_reward($_SESSION['num_user'])){
         let window_div = document.createElement("div");
         let topbar_div = document.createElement("div");
         let close_div  = document.createElement("div");
-        let equipe_pokemon_div = document.createElement("div");
-        let boite_pokemon_div = document.createElement("div");
-        equipe_pokemon_div.className = "equipe_pokemon_div";
-        boite_pokemon_div.className = "boite_pokemon_div";
+        let content = document.createElement("div");
         window_div.className = "window";
         topbar_div.className = "topbar";
         close_div.className = "close";
+        content.className = "window_content"
         document.querySelector("body").appendChild(window_div);
         document.querySelector(".window").appendChild(topbar_div);
         document.querySelector(".topbar").appendChild(close_div);
         document.querySelector(".close").innerHTML = 'x';
-        document.querySelector(".window").appendChild(equipe_pokemon_div);
-        document.querySelector(".window").appendChild(boite_pokemon_div);
-        openGereEquipe = 1;
+        document.querySelector(".window").appendChild(content);
         document.querySelector(".close").addEventListener("click",function ()
             { 
             document.querySelector("body").removeChild(document.querySelector(".window")); 
-            openGereEquipe = 0;
+            openFenetre = 0;
             });
 
+    }
+    function afficher_pokemon(typeAffichage,numPokemon){
+        if(typeAffichage == "miniature" || typeAffichage == "miniature_shiny"){
+            return "<img src=Data/pokemon/"+typeAffichage+"/"+numPokemon+".png>";
+        }
+        return "<img src=Data/pokemon/"+typeAffichage+"/"+numPokemon+".gif>"
     }
     function update_pokemon_div(){
         var xhr = new XMLHttpRequest();
@@ -170,14 +170,18 @@ if (need_reward($_SESSION['num_user'])){
         xhr.addEventListener('readystatechange', function () {
             if((xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200)){
                 var tab = JSON.parse(xhr.responseText);
-                text_pokemon = '<p>Equipe<p>';
+                text_pokemon = '';
                 for(var i = 0 ; i < 6 ; i++){
-                    console.log(tab[i]);
                     if(i<tab.length){
-                        text_pokemon = text_pokemon + "<div><p>"+tab[i]["Nom"]+"</p><p> lvl: "+tab[i]["Niveau"]+"</p></div>";
+                        text_pokemon=text_pokemon+"<div class=\"pokemon_div_equipe\"><div class=\"pokemon_div_equipe_gauche\"><div class=\"equipe_gauche_img\">";
+                        text_pokemon=text_pokemon+afficher_pokemon("miniature",tab[i]['Num_pokemon']) + "</div>";
+                        text_pokemon=text_pokemon+"<div class=\"equipe_gauche_lvl\">N."+tab[i]['Niveau']+"</div></div>";
+                        text_pokemon=text_pokemon+"<div class=\"pokemon_div_equipe_droit\"><div class=\"equipe_droit_nom\">";
+                        text_pokemon=text_pokemon+tab[i]['Nom']+"</div><div class=\"equipe_droit_pv\">";
+                        text_pokemon=text_pokemon+"PV"+"</div><div class=\"equipe_droit_numpv\">XXX/XXX</div></div></div>";
                     }
                     else {
-                        text_pokemon = text_pokemon + "<div><p>vide</p></div>"
+                        text_pokemon = text_pokemon + "<div class=\"pokemon_div_equipe\"></div>"
                     }
                 }
                 document.querySelector(".equipe_pokemon_div").innerHTML = text_pokemon;
@@ -193,22 +197,36 @@ if (need_reward($_SESSION['num_user'])){
                 var tab = JSON.parse(xhr.responseText);
                 text_pokemon = '<p>boite<p>';
                 for(var i = 0 ; i < tab.length ; i++){
-                    text_pokemon = text_pokemon + "<div><p>"+tab[i]["Nom"]+"</p><p> lvl: "+tab[i]["Niveau"]+"</p></div>";
+                    text_pokemon = text_pokemon + "<div class=\"boite_pokemon\">"+afficher_pokemon("miniature",tab[i]["Num_pokemon"])+"<p> "+tab[i]["Nom"]+"</p><p> lvl: "+tab[i]["Niveau"]+"</p></div>";
                 }
                 document.querySelector(".boite_pokemon_div").innerHTML = text_pokemon;
             }
         });
         xhr.send();
     }
+    function setFenetreGererEquipeP(){
+        let equipe_pokemon_div = document.createElement("div");
+        let boite_pokemon_div = document.createElement("div");
+        equipe_pokemon_div.className = "equipe_pokemon_div";
+        boite_pokemon_div.className = "boite_pokemon_div";
+        document.querySelector(".window_content").appendChild(equipe_pokemon_div);
+        document.querySelector(".window_content").appendChild(boite_pokemon_div);
+        openGereEquipe = 1;
+        document.querySelector(".close").addEventListener("click",function ()
+            { 
+            openGereEquipe = 0; 
+            });
+    }
     function afficher_gerer_equipe(){
-        if (openGereEquipe == 0){
+        if (openGereEquipe == 0 && openFenetre == 0){
             create_fenetre();
+            setFenetreGererEquipeP();
             update_pokemon_div();
             update_pokemon_boite();
         }
     }
     var openGereEquipe = 0;
-
+    var openFenetre = 0;
     update_social_div();
     setInterval(update_demande_reçus,5000);
     setInterval(update_friends_div,10000);
